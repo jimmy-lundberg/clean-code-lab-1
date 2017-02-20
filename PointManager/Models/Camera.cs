ce646831ab3a25ce4b01f44f28d3664f436bfc81
Jimmy Lundberg <jimmy-lundberg@hotmail.com>
2017-02-20 20:06:08 +0100

    Moved Maze project into PointManager project, changed start window to MazeWindow.

---
+++ PointManager/Models/Camera.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Windows.Media.Media3D;
+
+namespace PointManager.Models
+{
+    public class Camera
+    {
+        private const double halfPi = Math.PI / 180;
+        private Point3D _Position;
+        private double _HorizontalDegree, _VerticalDegree;
+
+        public Point3D Position { get { return _Position; } set { _Position = value; } }
+        public double HorizontalDegree { get { return _HorizontalDegree; } set { _HorizontalDegree = AngleInterval(value); } }
+        public double VerticalDegree { get { return _VerticalDegree; } set { _VerticalDegree = AngleInterval(value); } }
+
+        public double X { get { return _Position.X; } set { _Position.X = value; } }
+        public double Y { get { return _Position.Y; } set { _Position.Y = value; } }
+        public double Z { get { return _Position.Z; } set { _Position.Z = value; } }
+
+        public Point3D Look
+        {
+            get
+            {
+                const int distance = 3;
+                double X1 = Math.Sin(HorizontalDegree * halfPi) * distance, Z1 = Math.Cos(HorizontalDegree * halfPi) * distance;
+
+                return new Point3D()
+                {
+                    X = (Math.Cos(VerticalDegree * halfPi) * X1),
+                    Y = (Math.Sin(VerticalDegree * halfPi) * distance),
+                    Z = (Math.Cos(VerticalDegree * halfPi) * Z1)
+                };
+            }
+        }
+
+        public void Move(double Distance)
+        {
+            _Position.X += Math.Sin(HorizontalDegree * halfPi) * Distance;
+            _Position.Z += Math.Cos(HorizontalDegree * halfPi) * Distance;
+        }
+
+        public void Strafe(double Distance)
+        {
+            var dx = Math.Sin(HorizontalDegree * halfPi) * Distance;
+            var dz = Math.Cos(HorizontalDegree * halfPi) * Distance;
+            _Position.X += -dz;
+            _Position.Z += dx;
+        }
+
+
+        private double AngleInterval(double deg)
+        {
+            if (deg > 360) return deg - 360;
+            if (deg < 0) return deg + 360;
+            return deg;
+        }
+    }
+}
---
+++ PointManager/Views/MazeWindow.xaml.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Media.Media3D;
+using System.Windows.Input;
+using System.Windows.Threading;
+
+namespace PointManager.Views
+{
+    public partial class MazeWindow : Window
+    {
+        public MazeWindow()
+        {
+            InitializeComponent();
+        }
+
+        enum Movement
+        {
+            Neg = -1,
+            None = 0,
+            Pos = 1
+        }
+
+        PointManager.Models.Camera camera;
+        PerspectiveCamera perspectiveCamera = new PerspectiveCamera();
+        DispatcherTimer timer;
+        Movement Walk, Strafe;
+        double Steps = 1;
+
+        private void PrintCameraData()
+        {
+            TextBox_X.Text = (Math.Round(camera.X, 2)).ToString();
+            TextBox_Y.Text = (Math.Round(camera.Y, 2)).ToString();
+            TextBox_Z.Text = (Math.Round(camera.Z, 2)).ToString();
+            TextBox_Vertical.Text = (Math.Round(camera.VerticalDegree, 2)).ToString();
+            TextBox_Horizontal.Text = (Math.Round(camera.HorizontalDegree, 2)).ToString();
+        }
+
+        private void Window1_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.Key)
+            {
+                case Key.Up: Walk = Movement.Pos; break;
+                case Key.Down: Walk = Movement.Neg; break;
+                case Key.Left: Strafe = Movement.Neg; break;
+                case Key.Right: Strafe = Movement.Pos; break;
+                case Key.Z: camera.Y += 0.1; break;
+                case Key.X: camera.Y -= 0.1; break;
+            }
+        }
+
+        private void Window1_KeyUp(object sender, KeyEventArgs e)
+        {
+            switch (e.Key)
+            {
+                case Key.Up: Walk = Movement.None; break;
+                case Key.Down: Walk = Walk = Movement.None; break;
+                case Key.Left: Strafe = Movement.None; break;
+                case Key.Right: Strafe = Movement.None; break;
+            }
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            if (Walk != Movement.None)
+            {
+                camera.Move((double)Walk * Steps * 0.1);
+            }
+
+            if (Strafe != Movement.None)
+            {
+                camera.Strafe((double)Strafe * Steps * 0.1);
+            }
+
+            perspectiveCamera.Position = camera.Position;
+            perspectiveCamera.LookDirection = new Vector3D(camera.Look.X, camera.Look.Y, camera.Look.Z);
+            PrintCameraData();
+        }
+
+        private void Window1_Loaded(object sender, System.Windows.RoutedEventArgs e)
+        {
+            Viewport3D_World.Camera = perspectiveCamera;
+            camera = new PointManager.Models.Camera() { X = 1, Y = 0.5, Z = 0 }; //camera.HorizontalDegree = camera.VerticalDegree =0;
+            perspectiveCamera.Position = camera.Position;
+            perspectiveCamera.LookDirection = new Vector3D(camera.Look.X, camera.Look.Y, camera.Look.Z);
+            (new PointManager.Models.MazeGenerator()).MakeMaze(Model3DGroup_Lights);
+            timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromMilliseconds(16);
+            timer.Tick += new EventHandler(Timer_Tick);
+            this.timer.Start();
+        }
+
+        private void SetCameraAngles(Point point)
+        {
+            var midY = this.ActualHeight / 2;
+            // ned:  360-270.
+            if (point.Y > midY)
+            {
+                var proc = (point.Y - midY) / midY;
+                camera.VerticalDegree = 360 - 90 * proc;
+            }
+            // Vert: up:  0-90
+            if (point.Y < midY)
+            {
+                var proc = point.Y / midY;
+                camera.VerticalDegree = 90 - 90 * proc;
+            }
+            var proc2 = point.X / this.ActualWidth;
+            camera.HorizontalDegree = 720 - 720 * proc2;
+        }
+
+        private void Window1_MouseMove(object sender, MouseEventArgs e)
+        {
+            SetCameraAngles(e.GetPosition(null));
+        }
+    }
+}
